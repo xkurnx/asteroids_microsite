028ac7b95cd010f7752bc7dca9944d02fc84ac5b
xkurnx-pemula <xkurnx@gmail.com>
2014-03-26 17:29:47 +0700

    initial commit

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php 
+        /*
+         * Print the <title> tag based on what is being viewed.
+         */
+        global $page, $paged;
+    
+        wp_title( '|', true, 'right' );
+    
+        // Add the blog name.
+        bloginfo( 'name' );
+    
+        // Add the blog description for the home/front page.
+        $site_description = get_bloginfo( 'description', 'display' );
+        if ( $site_description && ( is_home() || is_front_page() ) )
+            echo " | $site_description";
+    ?>
+</title>
+
+<script type="text/javascript">
+// Locate the url of the proxy url.
+var memberProxyUrl = 'http://www.topcoder.com/roadshow/wp-content/plugins/home-eoi-plugin/mcount.php';
+</script>
+<link rel="stylesheet" type="text/css" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/social_widget.css" />
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/style.css" type="text/css" />
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/screen.css" type="text/css" />
+<link rel="stylesheet" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/interactive.css" type="text/css" />
+<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css' />
+<link type="text/css" href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/wordpress-default.css" rel="stylesheet" media="all" />
+<!--[if lte IE 8]> 
+        <link href="<?php bloginfo( 'stylesheet_directory' ); ?>/css/screen-ie8.css" type="text/css" rel="stylesheet" media="screen" />
+<![endif]--> 
+
+
+<script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/jquery-1.5.2.min.js" type="text/javascript"></script>
+<script src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/jquery.jcarousel.js" type="text/javascript"></script>
+
+<script type="text/javascript" src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/script.js"></script>
+<script type="text/javascript" src="<?php bloginfo( 'stylesheet_directory' ); ?>/js/interactive.js"></script>
+<?php wp_head(); ?>
+</head>
+
+<body>
+<!-- wrapper -->
+<div id="wrapper">
+  <div id="innerWrapper"> 
+    <!-- header -->
+    <div id="header"> 
+      <?php 
+	  wp_nav_menu( 
+        array(
+          'container' => false,
+          'theme_location' => '',
+          'menu_class' => 'topMenu',
+          'menu' => 'top',
+          'link_before' => '<span class="right"><span class="middle">',
+          'link_after' => '</span></span>',
+
+        ) 
+      ); 
+	  ?>
+    	<a id="siteLogo" href="http://community.topcoder.com/ntl">
+			<img src="<?php bloginfo("stylesheet_directory");?>/i/NTL_Logo.png" alt=""/>
+		</a>
+    <span class="shadowSep"></span>
+		<a>
+			<img src="<?php bloginfo("stylesheet_directory");?>/i/asteroid_logo.png" alt=""/>
+		</a>
+      
+      
+    </div>
